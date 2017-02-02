797b318a1966f82d049c3b631b3003de7b2baacb
Joshua Bradford <jjyet@yahoo.com>
2017-02-02 14:37:04 -0500

    added questions and directions as comments in main.js file added html so that we have all 6 questions now

---
+++ main.js
@@ -0,0 +1,90 @@
+/*
+Use the items variable to write a function for each question that gives the answer.
+Using the innerHTML() method, place your answers in the correct part of the index.html file.
+Make your index.html page look just like the mockup.png (shouldn't be too bad).
+Make sure the output matches the answers below each question.
+Make sure to comment your code as you walk through your though process.
+*/
+
+//Show me how to calculate the average price of all items.
+//  The average price is $23.63
+
+
+
+
+/*Show me how to get an array of items that cost between $14.00 and $18.00 USD
+  1970s Coors Banquet Glass Beer Pitcher
+
+  The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug
+
+  Hand Painted Colorful Feather Glass*/
+
+
+
+
+
+
+
+/*Which item has a "GBP" currency code? Display it's name and price.
+  1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18 */
+
+
+
+
+/*
+Display a list of all items who are made of wood.
+  SALE Mid Century Siesta Ware White Mug with Anchor - Set of 3 is made of wood.
+
+  Bottle cap catcher personalized. Man cave gift for him- Wooden Beer pub sign - Groomsmen wedding Gift is made of wood.
+
+  Medium Size, Welcome To Our Firepit-Where Friends And Marshmallows Get Toasted At The Same Time-Painted Wood Sign-Custom Colors is made of wood.
+
+  Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
+
+  Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.
+*/
+
+
+
+
+
+
+
+
+/*
+Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+  Qty of 2 Groomsmen Gift - Stainless Steel Personalized Bottle Opener - NO Capcatcher has 9 materials:
+  wall mount bottle opener
+  wedding
+  man cave
+  christmas gift
+  guy gift
+  fathers day
+  home bar
+  beer
+  bar
+
+  The Three Broomsticks Customizable Beer Stein Mug, Harry Potter  Inspired, hogsmeade village, harry potter gift, three broomsticks mug  has 13 materials:
+
+  glass
+  sandblast cabinet
+  vinyl
+  beer glass
+  pint glass
+  etched pint glass
+  etched glass
+  etched beer glass
+  16 oz pint
+  beer gift
+  etched harry potter glass
+  the three broomsticks glass
+  personalized harry potter glass
+*/
+
+
+
+
+/*
+How many items were made by their sellers?
+  18 were made by their sellers
+*/
